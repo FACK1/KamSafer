8d57134f81de0a4299055c7ca5ff0bc8831a4a4f
shaima96 <141062@ppu.edu.ps>
2019-02-12 16:05:01 -0600

    make style for header component

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,19 +1,78 @@
 import React from "react";
 import KS_logo from "./KS_logo.png";
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 import Logout from "./Logout.png";
-import Replay from "./Reply.png";
-import Typography from "@material-ui/core/Typography";
-const Header = () => {
+import Reply from "./Reply.png";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles =
+{
+  position:
+  {
+    display:"flex",
+    justifyContent:"center",
+    backgroundColor:"#FCE5E9",
+    width:"100vw",
+  },
+
+  logo1:
+  {
+    width:"3rem",
+    height:"3rem",
+  },
+  grow:
+  {
+    color:"#A11010",
+    fontSize:"2rem",
+    fontWeight:"bold"
+  },
+
+  container:
+  {
+    display: "flex",
+    flexDirection:"column",
+    alignItems: "center"
+  },
+
+  logo3:
+  {
+    width:"2rem",
+    height:"2rem",
+  },
+  logo2:
+  {
+
+  alignSelf:"flex-end",
+    width:"2.5rem",
+    height:"2.5rem",
+  }
+
+};
+const Header = ({classes,location}) => {
+  const {pathname}=location
   return (
-    <div className="header">
-      <div>
-        <img className="exit" alt="logo" src={Logout} />
-      </div>
-      <div>
-        <img className="logo" alt="logo" src={KS_logo} />
-        <h1>KamSafer</h1>
-      </div>
-    </div>
+    <div className={classes.position}>
+
+       <Toolbar >
+        {!pathname==="/cars" &&  <div>
+         <img  className={classes.logo3} alt="logo" src={Reply}/>
+       </div>}
+       <div className={classes.container}>
+        <div>
+          <img  className={classes.logo1} alt="logo" src={KS_logo}/>
+        </div>
+        <Typography  variant="h2"  className={classes.grow}>
+          KamSafer
+        </Typography >
+        </div>
+        <div>
+          <img  className={classes.logo2} alt="logo" src={Logout}/>
+        </div>
+
+         </Toolbar>
+
+     </div>
   );
 };
-export default Header;
+export default withStyles(styles)(Header);
